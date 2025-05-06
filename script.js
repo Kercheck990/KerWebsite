ec042c0c47ac5acc1d01c80e8b400fcd98671e40
Kercheck990 <kirilmajergrove@gmail.com>
2025-05-06 11:40:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const ADMIN_PASSWORD = "supersecret"; // Замени на свой пароль
+const ADMIN_PASSWORD = "qwerty123"; // Замени на свой пароль
 
 function login() {
   const password = document.getElementById("adminPassword").value;
+  const errorMessage = document.getElementById("loginError");
+
   if (password === ADMIN_PASSWORD) {
     document.getElementById("loginContainer").classList.add("hidden");
     document.getElementById("mainContainer").classList.remove("hidden");
   } else {
-    document.getElementById("loginError").innerText = "Неверный пароль!";
+    errorMessage.classList.remove("hidden");
   }
 }
