25ef95478cd97adbb0d78bad33920040987817da
Kercheck990 <kirilmajergrove@gmail.com>
2025-08-26 11:56:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 
     // --- НАСТРОЙКИ ДЛЯ ПРИВЯЗКИ ТГ ---
     const GITHUB_REPO = 'Kercheck990/KerWebsite';
-    const GITHUB_TOKEN = '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'; // !!! ВСТАВЬ СЮДА СВОЙ ТОКЕН !!!
+    const GITHUB_TOKEN = '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'; // !!! ВСТАВЬ СЮДА СВОЙ ТОКЕН !!!
     const PASSWORDS_FILE = 'passwords.json';
 
     // Тема
